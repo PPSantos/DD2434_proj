7bd0cf6c7ba9554d227d28cce1d3bf5e8a20a1d1
Pierre MARZA <marza@kth.se>
2019-01-09 19:10:05 +0100

    sinus cardinal dataset + linear spline kernel implemented

--- src/RVM.py
+++ src/RVM.py
@@ -9,21 +9,32 @@ class RVM:
         # Prior variance.
         self.alphas = None
         # Likelihood variance.
-        self.sigma = 1
+        self.sigma = 0.01**2
 
         self.phi = None
         self.T = None
         self.X_train = None
 
-        self.threshold_alpha = 1e9
+        self.threshold_alpha = 1e5
         
         self.removed_bias = False
 
 
     def kernel(self, x, y):
         #return linear_kernel(x, y)
-        return rbf_kernel(x, y, 0.01)
-
+        #return rbf_kernel(x, y, 0.1)
+        return self.linear_spline_kernel(x, y)
+    
+    def linear_spline_kernel(self, X, Y):
+        phi = np.zeros((X.shape[0], Y.shape[0]))
+        
+        for i in range(phi.shape[0]):
+            for j in range(phi.shape[1]):
+                x = X[i]
+                y = Y[j]
+                phi[i,j] = 1 + x*y + x*y*min(x,y) - ((x+y)*min(x,y)**2)/2 + np.power(min(x,y),3)/3
+        
+        return phi
     #def linear_kernel(self, x, y):
     #    return np.dot(x, y.T)
 
@@ -76,8 +87,6 @@ class RVM:
             
     def em(self):
         
-        difference = 5
-        
         while(True):
             
             old_alphas = np.copy(self.alphas)
@@ -88,24 +97,32 @@ class RVM:
             self.alphas = gammas / (mu_posterior**2)
 
             # Update sigma.
-            N = self.alphas.shape[0] - 1
+            '''N = self.alphas.shape[0] - 1
             self.sigma = (N - np.sum(gammas))/(
                     np.sum((T - np.dot(self.phi, mu_posterior)) ** 2))
             #print(self.sigma)
+            '''
             
             difference = np.amax(np.abs(self.alphas - old_alphas))
             
             if difference < 1e-3:
+                print("EM finished")
                 break
 
             self.prune()
+            
+    def get_relevance_vectors(self):
+        return self.X_train
 
 
     def predict(self, X):
 
         sigma_posterior, mu_posterior = self.calc_posterior()
         
-        print("Nb relevant features : ", mu_posterior.shape[0])
+        nb_relev_vect = mu_posterior.shape[0]
+        if not self.removed_bias :
+            nb_relev_vect -= 1
+        print("Nb relevance vectors : ", nb_relev_vect)
 
         phi = self.kernel(X, self.X_train)
         
@@ -124,23 +141,33 @@ class RVM:
         return y
 
 # test.
-N = 50
+N = 500
 
-X = np.linspace(0,10,N)
-Y = 2*X + 100
-T = Y + np.random.randn(N)
+X = np.linspace(-10,10,N)
+#Y = 2*X + 100
+Y = np.sinc(X)
+T = Y + np.random.randn(N) * 0.01**2
 
 X = X.reshape(N,1)
 
 #plt.scatter(X,T)
 #plt.show()
 
+plt.scatter(X, T)
+plt.show()
+
 rvm = RVM()
 
 rvm.fit(X,T)
 
+relevance_vect = rvm.get_relevance_vectors()
+indexes_relev_vect = [i for i,x in enumerate(X) if x in relevance_vect]
+
 y_pred = rvm.predict(X)
 
-plt.plot(X, y_pred)
-plt.scatter(X, T, c='r')
+
+
+plt.plot(X, y_pred, c='r')
+plt.scatter(X, T)
+#plt.scatter(relevance_vect, T[indexes_relev_vect], c='g', marker='X', linewidth=12)
 plt.show()
